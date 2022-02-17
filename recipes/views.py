74c31c20f08f9f400756806785305c56f9009164
Anderson Pitagoras <anderson.pitagoras@gmail.com>
2022-02-17 20:57:56 -0300

    removido contato sobre

--- recipes/views.py
+++ recipes/views.py
@@ -4,11 +4,3 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/home.html')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
-
-
-def contato(request):
-    return HttpResponse('Contato')
